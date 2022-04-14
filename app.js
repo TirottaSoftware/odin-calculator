089bb788e3d4d492801e01b7bb6f5f391cecdf98
Cristian <i449915@student.fontys.nl>
2022-04-14 11:00:39 +0300

    Implemented basic calculator functions

--- app.js
+++ app.js
@@ -1,9 +1,52 @@
+//Get all DOM elements
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll(".btn-num");
+const operators = document.querySelectorAll(".operator-buttons button");
+const enterButton = document.querySelector(".btn-enter");
+
+let displayValue = "";
+let num1 = null;
+let num2 = null;
+let operator = "";
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    let buttonContent = button.textContent;
+    displayValue += buttonContent.toString();
+    console.log(displayValue);
+  });
+});
+
+operators.forEach((button) => {
+  // save num1 input
+  // clear display
+  // save operator value
+  // start listening for num2 inputs
+  // if num1 has been declared, just change the operator
+  button.addEventListener("click", () => {
+    if (!num1) {
+      num1 = displayValue;
+      displayValue = "";
+    }
+    operator = button.textContent;
+    console.log(operator);
+  });
+});
+
+enterButton.addEventListener("click", () => {
+  num2 = displayValue;
+  console.log(`${num1}${operator}${num2}`);
+  operate(operator, num1, num2);
+});
+
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
 function operate(operator, a, b) {
+  a = parseInt(a);
+  b = parseInt(b);
   switch (operator) {
     case "+":
       console.log(add(a, b));
@@ -15,9 +58,14 @@ function operate(operator, a, b) {
       console.log(multiply(a, b));
       break;
     case "/":
-      console.log(divide(a, b));
+      a == 0 || b == 0
+        ? console.log("Cannot divide by 0")
+        : console.log(divide(a, b));
       break;
     default:
       break;
   }
+  displayValue = "";
+  num1 = null;
+  num2 = null;
 }
